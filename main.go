0e345f7e4805a92e99544df91fd34d423f705000
mohuishou <1@lailin.xyz>
2018-01-03 22:00:31 +0800

    update

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/gocolly/colly"
-)
-
 func main() {
-	c := colly.NewCollector()
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		log.Println(e.Text)
-		s, er := e.DOM.Html()
-		log.Println("lalal:", s, er)
-	})
-
-	c.OnResponse(func(resp *colly.Response) {
-		log.Println("lalal2:", string(resp.Body))
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Add("Referer", "http://scuinfo.com/")
-	})
-
-	c.Visit("http://scuinfo.com/api/posts?pageSize=15")
-	c.Wait()
 }
