181752f344f0da11692e1e69eb6c59c85bcb84ea
e <1@lailin.xyz>
2018-01-04 19:09:56 +0800

    update

--- model/model.go
+++ model/model.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // mysql驱动
 	"github.com/mohuishou/scuplus-spider/log"
 )
 
@@ -21,13 +21,14 @@ type Model struct {
 // init 数据库初始化
 func initDB() {
 	var err error
-	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "123456", "localhost", "32770", "scuplus-spider"))
+	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "123456", "localhost", "3306", "scuplus-spider"))
 	if err != nil {
 		log.Fatal("数据库连接错误：", err)
 	}
 	db.AutoMigrate(&Detail{Model: Model{}}, &Tag{Model: Model{}}, &DetailTag{Model: Model{}})
 }
 
+// DB 返回db，如果不存在则初始化
 func DB() *gorm.DB {
 	if db == nil {
 		initDB()
